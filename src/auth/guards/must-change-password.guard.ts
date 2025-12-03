e487ac3c83b3e3b9800fb4d3bfbe5d03a6c08c9d
Le Hoang Tuan <tuan.le@gss-sol.com>
2025-12-04 02:51:37 +0700

    format code

--- src/auth/guards/must-change-password.guard.ts
+++ src/auth/guards/must-change-password.guard.ts
@@ -16,28 +16,28 @@ interface JwtUser {
 export class MustChangePasswordGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     // Ép kiểu request + user type-safe
-  //   const request = context
-  //     .switchToHttp()
-  //     .getRequest<Request & { user: JwtUser }>();
-  //   const user = request.user;
+    //   const request = context
+    //     .switchToHttp()
+    //     .getRequest<Request & { user: JwtUser }>();
+    //   const user = request.user;
 
-  //   if (!user) {
-  //     throw new ForbiddenException('Unauthorized');
-  //   }
+    //   if (!user) {
+    //     throw new ForbiddenException('Unauthorized');
+    //   }
 
-  //   const allowedPath: string = '/auth/change-password-first';
-  //   const actualPath: string = request.path;
+    //   const allowedPath: string = '/auth/change-password-first';
+    //   const actualPath: string = request.path;
 
-  //   if (user.mustChangePassword) {
-  //     if (actualPath === allowedPath) {
-  //       return true;
-  //     }
-  //     throw new ForbiddenException('Bạn phải đổi mật khẩu trước khi tiếp tục.');
-  //   }
+    //   if (user.mustChangePassword) {
+    //     if (actualPath === allowedPath) {
+    //       return true;
+    //     }
+    //     throw new ForbiddenException('Bạn phải đổi mật khẩu trước khi tiếp tục.');
+    //   }
 
-  //   return true;
-  // }
-   const request = context
+    //   return true;
+    // }
+    const request = context
       .switchToHttp()
       .getRequest<Request & { user: JwtUser }>();
     const user = request.user;
